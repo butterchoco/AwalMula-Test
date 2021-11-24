d0d66e252d55a7cc757994357c03262700143ec5
butterchoco <supri.contact@gmail.com>
2021-11-24 11:18:44 +0700

    add productsID page

---
+++ pages/products/[id].js
@@ -0,0 +1,14 @@
+import LayoutDefault from "@/Components/layout/default";
+import { Heading, Image } from "@chakra-ui/react";
+
+const productsId = () => {
+  return (
+    <LayoutDefault title="Products">
+      <Heading as="h3" size="lg">
+        Test
+      </Heading>
+    </LayoutDefault>
+  );
+};
+
+export default productsId;
